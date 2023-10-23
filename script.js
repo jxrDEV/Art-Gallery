3b45c22f968983e5df03b41a71693629cfb34635
jxrDEV <johnrhovicjxr@gmail.com>
2023-10-23 09:40:27 +0800

    i replaced the art images

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ window.onscroll = function () {
   const opacity = distanceFromBottom / windowHeight;
   textElement.style.opacity = Math.max(0, opacity);
 }
+
+let arts = [
+  {
+    artName1: 'Art-1',
+    artSource1: "arts/art-1.png"
+  },
+
+  {
+    artName2: 'Art-2',
+    artSource2: "arts/art-2.png"
+  },
+
+  {
+    artName3: 'Art-3',
+    artSource3: "arts/art-3.png"
+  },
+
+  {
+    artName4: 'Art-4',
+    artSource4: "arts/art-1.png"
+  }
+];
+
+
+
